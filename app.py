6c7473f50702f6776067037afc4ec10197d2c0b5
lemellofonseca <54309520+lemellofonseca@users.noreply.github.com>
2024-06-19 20:26:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return 'Bem vindo a Gulliver Traveller!'
+@app.route('/')
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=5000)
